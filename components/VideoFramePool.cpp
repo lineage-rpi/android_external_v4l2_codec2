01afb201ec3dae3dfd0cb984fd34a00ac416c636
Chih-Yu Huang <akahuang@google.com>
2020-10-05 14:25:04 +0900

    VideoFramePool: Remove CPU_READ usage for buffers used in surface mode
    CPU access to buffers is not required in surface mode. This change
    makes VideoFramePool not set CPU_READ usage flag for surface mode,
    to allow for more efficient buffers to be used.
    
    Bug: 169212117
    Test: pass tast.arc.VideoDecodeAccel.vp8_vm
    Change-Id: I893096a89a54d8784def3650b4a8985e5decbd8d

--- components/VideoFramePool.cpp
+++ components/VideoFramePool.cpp
@@ -76,8 +76,15 @@ std::unique_ptr<VideoFramePool> VideoFramePool::Create(
         scoped_refptr<::base::SequencedTaskRunner> taskRunner) {
     ALOG_ASSERT(blockPool != nullptr);
 
-    C2MemoryUsage memoryUsage((isSecure ? C2MemoryUsage::READ_PROTECTED : C2MemoryUsage::CPU_READ) |
-                              static_cast<uint64_t>(BufferUsage::VIDEO_DECODER));
+    uint64_t usage = static_cast<uint64_t>(BufferUsage::VIDEO_DECODER);
+    if (isSecure) {
+        usage |= C2MemoryUsage::READ_PROTECTED;
+    } else if (blockPool->getAllocatorId() == android::V4L2AllocatorId::V4L2_BUFFERPOOL) {
+        // CPU access to buffers is only required in byte buffer mode.
+        usage |= C2MemoryUsage::CPU_READ;
+    }
+    const C2MemoryUsage memoryUsage(usage);
+
     if (requestNewBufferSet(*blockPool, numBuffers, size, static_cast<uint32_t>(pixelFormat),
                             memoryUsage) != C2_OK) {
         return nullptr;
